4bb4b8ce62ee9b410e826cd29c258874d22ad9db
Lena Voytek <lena@voytek.dev>
2024-03-03 18:39:55 -0700

    Add default glow on startup

--- xc3-powerframe/src/main.cpp
+++ xc3-powerframe/src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define NEOPIXEL_DATA_PIN 3
+#define NEOPIXEL_RING_LENGTH 45
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXEL_RING_LENGTH, NEOPIXEL_DATA_PIN, NEO_GRB + NEO_KHZ800);
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
 }
 
-void loop() {
+void standardSolidBlue() {
+  colorWipe(strip.Color(24, 239, 255), 25);
+}
+
+void setup()
+{
+  strip.begin();
+  strip.setBrightness(20);
+  strip.show();
+  standardSolidBlue();
+}
+
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
